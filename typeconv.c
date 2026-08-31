b121a21afe7b4773d62c56a3e21b98e67470f97b
govindjoshy77-dev <govindjoshy77@gmail.com>
2026-08-31 20:11:40 +0530

    Add files via upload

---
+++ typeconv.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i=65;
+	float f=6.2;
+	double d=28.689;
+	char c='O';
+	
+	printf("*********Type Conversion***********\n");
+	printf("Integer to float = %f\n",(float)i);
+	printf("float to Integer = %d\n",(int)f);
+	printf("Float to Double = %lf\n",(double)f);
+	printf("Double to float = %f\n",(float)d);
+	printf("Char to Integer = %d\n",(int)c);
+	printf("Integer to Char= %c\n",(char)i);
+
+
+}
---
+++ typedef.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#define PI 3.14
+void main()
+{
+	typedef float jamal;
+	jamal r,area;
+	printf("Enter the radius :");
+	scanf("%f",&r);
+	area=2*PI*r;
+	printf("area is : %f ",area);
+}
